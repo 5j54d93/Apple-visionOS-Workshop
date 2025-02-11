9b3c66a4922f94c8d4603541178fd88395593045
Ricky <RickyChuang.work@gmail.com>
2025-02-11 16:57:37 +0800

    Initial Commit

---
+++ Apple-visionOS-Workshop/Apple_visionOS_WorkshopApp.swift
@@ -0,0 +1,33 @@
+//
+//  Apple_visionOS_WorkshopApp.swift
+//  Apple-visionOS-Workshop
+//
+//  Created by Ricky on 2025/2/11.
+//
+
+import SwiftUI
+
+@main
+struct Apple_visionOS_WorkshopApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
